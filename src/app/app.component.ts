5341119b9899c37c2b93f29f4ec5fbbbf7725088
Nazir Ahmad <nazircinu@gmail.com>
2018-08-26 11:49:15 +0530

    Refactored navigation into separate component-update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,25 +1,5 @@
 import { Component, ElementRef, ViewChild, TemplateRef, HostListener, Renderer2, ViewChildren, Directive, Output, EventEmitter, Input, OnChanges } from '@angular/core';
 
-@Directive({
-  selector: '.nav-link'
-})
-export class NavigationDir implements OnChanges {
-
-  constructor(private element: ElementRef, private renderer: Renderer2) {
-
-  }
-
-  @Input() clickedLinkText:string;
-
-  ngOnChanges(){
-    this.renderer.removeClass(this.element.nativeElement,"activeLink");
-    if(this.element.nativeElement.innerText==this.clickedLinkText){
-      this.renderer.addClass(this.element.nativeElement,"activeLink");
-    }    
-  }
-}
-
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
